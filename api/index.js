3550e3aa997ef31090ebefc6964f8d1ac9b94669
tauheedbuttt <tauheedbutt@gmail.com>
2025-10-15 20:41:43 +0300

    refactor: use path module to construct absolute path for handler

--- api/index.js
+++ api/index.js
@@ -1,4 +1,21 @@
-const handler = require('./apps/backend/dist/main.js').default;
+// org/api/index.js
+
+// This uses Node's path module to construct an absolute path
+// that should reliably point to the included built file.
+const path = require('path');
+
+// __dirname is /var/task/api. We move up one level (..) to the task root,
+// then follow the absolute path to your compiled app.
+const mainPath = path.resolve(
+  __dirname,
+  '..',
+  'apps',
+  'backend',
+  'dist',
+  'main.js'
+);
+
+const handler = require(mainPath).default;
 
 module.exports = (req, res) => {
   return handler(req, res);
